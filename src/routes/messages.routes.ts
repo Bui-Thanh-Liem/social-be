2b65c8ea8bdf317aece5ffa0de38f12cff4842da
liemdev <buithanhliem5073@gmail.com>
2025-11-05 11:08:34 +0700

    update code

--- src/routes/messages.routes.ts
+++ src/routes/messages.routes.ts
@@ -3,7 +3,6 @@ import MessagesController from '~/controllers/Messages.controller'
 import { requestParamsValidate } from '~/middlewares/request-params-validate.middleware'
 import { requestQueryValidate } from '~/middlewares/request-query-validate.middleware'
 import { verifyAccessToken } from '~/middlewares/verify-access-token.middleware'
-import { verifyUserEmail } from '~/middlewares/verify-user-email.middleware'
 import { GetMultiMessageByConversationDtoSchema } from '~/shared/dtos/req/message.dto'
 import { QueryDtoSchema } from '~/shared/dtos/req/query.dto'
 import { wrapAsyncHandler } from '~/utils/wrap-async-handler.util'
@@ -13,7 +12,6 @@ const messagesRoute = Router()
 messagesRoute.get(
   '/:conversation_id',
   verifyAccessToken,
-  verifyUserEmail,
   requestParamsValidate(GetMultiMessageByConversationDtoSchema),
   requestQueryValidate(QueryDtoSchema),
   wrapAsyncHandler(MessagesController.getMultiByConversation)
