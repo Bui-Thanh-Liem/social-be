9938b8d1a74ecca1a92656066c84a640b781978d
liemdev <buithanhliem5073@gmail.com>
2025-11-18 16:51:30 +0700

    update code

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -2,5 +2,5 @@ import { envs } from './env.config'
 
 export const mongodbConnection =
   envs.NODE_ENV === 'production'
-    ? `mongodb://${envs.DB_USERNAME}:${envs.DB_PASSWORD}@${envs.DOMAIN}:27017/${envs.DB_NAME}?authSource=${envs.DB_NAME}&replicaSet=rs0`
+    ? `mongodb://${envs.DB_USERNAME}:${envs.DB_PASSWORD}@127.0.0.1:27017/${envs.DB_NAME}?authSource=${envs.DB_NAME}&replicaSet=rs0`
     : `mongodb+srv://${envs.DB_USERNAME}:${envs.DB_PASSWORD}@cluster0-liemdev.dfynfof.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-LiemDev`
