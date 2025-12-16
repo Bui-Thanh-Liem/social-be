eff6304ca13769fc470824b83f093c3ad0d44cdb
liemdev <buithanhliem5073@gmail.com>
2025-12-16 16:45:54 +0700

    remove tag v10

--- src/tasks/sync.task.ts
+++ src/tasks/sync.task.ts
@@ -3,10 +3,10 @@ import { syncQueue } from '~/bull/queues'
 import { CONSTANT_JOB } from '~/shared/constants'
 
 /**
- * 🧹 JOB 1 — Mỗi 2 phút: lấy dữ liệu trong cache để đồng bộ xuống database
+ * 🧹 JOB 1 — Mỗi 1 phút: lấy dữ liệu trong cache để đồng bộ xuống database
  */
-cron.schedule('*/2 * * * *', async () => {
-  console.log('[CRON-2M] 🧹 Sync likes from cache to DB...')
+cron.schedule('*/1 * * * *', async () => {
+  console.log('[CRON-1M] 🧹 Sync likes from cache to DB...')
   syncQueue.add(CONSTANT_JOB.SYNC_LIKE, {})
-  console.log('[CRON-2M] ✅ Synced likes from cache to DB')
+  console.log('[CRON-1M] ✅ Synced likes from cache to DB')
 })
