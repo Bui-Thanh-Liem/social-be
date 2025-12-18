ca30cf36d319a552b7909682e8c8b0931af843fe
liemdev <buithanhliem5073@gmail.com>
2025-12-18 14:45:24 +0700

    remove tag v10

--- src/bull/queues/cleanup.queue.ts
+++ src/bull/queues/cleanup.queue.ts
@@ -1,4 +1,4 @@
-import { Queue } from 'bullmq'
+import { JobProgress, Queue, QueueEvents } from 'bullmq'
 import { redisConnection } from '~/configs/redis.config'
 import { CONSTANT_QUEUE } from '~/shared/constants'
 import { configDefaultJobOptions } from './job.conf'
@@ -10,5 +10,16 @@ export const cleanupQueue = new Queue(CONSTANT_QUEUE.CLEANUP, {
   }
 })
 
-// 🔹 Dev / staging / app nhỏ → Multi-worker single process
-// 🔹 Production / hệ thống lớn → Dedicated worker process (1 queue = 1 process).
+const queueEvents = new QueueEvents(CONSTANT_QUEUE.CLEANUP)
+
+queueEvents.on('completed', (jobId, result) => {
+  console.log(`Job ${jobId} completed ${result}`)
+})
+
+queueEvents.on('failed', (jobId, failedReason) => {
+  console.log(`Job ${jobId} failed ${failedReason}`)
+})
+
+queueEvents.on('progress', (e: { jobId: string; data: JobProgress }) => {
+  console.log(`Job ${e.jobId} progress: ${e.data}`)
+})
--- src/bull/queues/notification.queue.ts
+++ src/bull/queues/notification.queue.ts
@@ -1,4 +1,4 @@
-import { Queue } from 'bullmq'
+import { JobProgress, Queue, QueueEvents } from 'bullmq'
 import { redisConnection } from '~/configs/redis.config'
 import { CONSTANT_QUEUE } from '~/shared/constants'
 import { configDefaultJobOptions } from './job.conf'
@@ -8,4 +8,18 @@ export const notificationQueue = new Queue(CONSTANT_QUEUE.NOTIFICATION, {
   defaultJobOptions: {
     ...configDefaultJobOptions
   }
+})
+
+const queueEvents = new QueueEvents(CONSTANT_QUEUE.NOTIFICATION)
+
+queueEvents.on('completed', (jobId, result) => {
+  console.log(`Job ${jobId} completed ${result}`)
+})
+
+queueEvents.on('failed', (jobId, failedReason) => {
+  console.log(`Job ${jobId} failed ${failedReason}`)
+})
+
+queueEvents.on('progress', (e: { jobId: string; data: JobProgress }) => {
+  console.log(`Job ${e.jobId} progress: ${e.data}`)
 })
